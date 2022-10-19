19f63d9979828d6464e620c9517d953f099e149a
DDD <Sabitov.daniyar18@mail.ru>
2022-10-19 18:50:32 +0300

    Finished Sign in test on github

---
+++ src/test/java/com/sabitov/SignInTest.java
@@ -0,0 +1,86 @@
+package com.sabitov;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class SignInTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    JavascriptExecutor js;
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Web-Driver\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://github.com/");
+        //ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.id("login_field")).clear();
+        driver.findElement(By.id("login_field")).sendKeys("BestUazDriver");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Dearfriend18");
+        driver.findElement(By.name("commit")).click();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
